acab3d3ee9052a08e960156388c1a6946c9ed848
ShurikN57 <florian.machouche@yahoo.fr>
2026-08-09 11:05:19 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='breaking4-v3.0.12';
-const SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()))});
-self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(event.request,copy)).catch(()=>{});return res}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html'))))});
