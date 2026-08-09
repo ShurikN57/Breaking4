3ff9249f400b5ef9d32e14ef4159a973885e7884
ShurikN57 <florian.machouche@yahoo.fr>
2026-08-09 12:36:40 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='breaking4-v3.1.3';
+const SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(event.request,copy)).catch(()=>{});return res}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html'))))});
